6933117724f23ec12b8ba9bf5bd69753f2222fee
Akbar300 <Akbar@ecybertekschool.com>
2022-09-29 14:18:01 -0400

    @track decorator for object

--- force-app/main/default/lwc/dataBindingObjectArray/dataBindingObjectArray.js
+++ force-app/main/default/lwc/dataBindingObjectArray/dataBindingObjectArray.js
@@ -1,8 +1,10 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 
 export default class DataBindingObjectArray extends LightningElement {
   // create a property for location
   //type object and has 2 keys for city and state
+
+  @track // track decorator can be used to add interactivity to objects and array
   location = {
     city: "LA",
     state: "California"
@@ -10,6 +12,19 @@ export default class DataBindingObjectArray extends LightningElement {
 
   // create a method to handle the onchange event
   handleLocationChange(event) {
+    // event.target will return the element where the event is fired from
+    // and you can access it's attribute like value , labels or any other attributes
     console.log(event.target.value);
+    // print out the name attribute of the element that fired the event
+    console.log(event.target.name);
+
+    // Create a variavle to store above value for element name , var , let , const
+    let elementName = event.target.name;
+    // == will check for value only and === will check for value and type
+    if (elementName === "city") {
+      this.location.city = event.target.value;
+    } else if (elementName === "state") {
+      this.location.state = event.target.value;
+    }
   }
 }
