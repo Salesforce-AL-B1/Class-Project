9c43ffcb55a60501a0b17da42dc21b7e6533d7bf
Akbar300 <Akbar@ecybertekschool.com>
2022-10-03 12:59:59 -0400

    Lightning data service

--- force-app/main/default/lwc/app/app.js
+++ force-app/main/default/lwc/app/app.js
@@ -1,23 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
 export default class App extends LightningElement {
-
-   bike = {
-        name : 'Electra X4',
-        description : 'A sweet bike built for comfort.',
-        category : 'Mountain',
-        material : 'Steel',
-        price : '$2,700',
-        pictureUrl : 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg'
-    }
-   // simulate page load with forced 3 seconds wait 
-   ready = false;
-   // This is a lifecycle hook method that run right before the component added to html dom
-   // we are using it here to add 3 second wait time before it's added to html dom
-   connectedCallback() {
-       setTimeout(() => {
-           this.ready = true;
-       }, 3000);
-   }
-
+  bike = {
+    name: "Electra X4",
+    description: "A sweet bike built for comfort.",
+    category: "Mountain",
+    material: "Steel",
+    price: "$2,700",
+    pictureUrl:
+      "https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg"
+  };
+  // simulate page load with forced 3 seconds wait
+  ready = false;
+  // This is a lifecycle hook method that run right before the component added to html dom
+  // we are using it here to add 3 second wait time before it's added to html dom
+  connectedCallback() {
+    setTimeout(() => {
+      this.ready = true;
+    }, 3000);
+  }
 }
--- force-app/main/default/lwc/bike/bike.js
+++ force-app/main/default/lwc/bike/bike.js
@@ -1,10 +1,7 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 export default class Bike extends LightningElement {
-
-    // this will make it public property for communicating from app component to bike component 
-    @api childBike; 
-    // this propery is getting filled up by the bike object from app.js 
-    
-
+  // this will make it public property for communicating from app component to bike component
+  @api childBike;
+  // this propery is getting filled up by the bike object from app.js
 }
