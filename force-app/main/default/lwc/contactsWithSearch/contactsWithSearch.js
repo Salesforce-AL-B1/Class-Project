23decbc263aa1d095d59b3963a3f8eba293a7e62
Akbar300 <Akbar@ecybertekschool.com>
2022-10-06 12:24:49 -0400

    Wiring Apex method with Parameter Pass parameter by user's input

--- force-app/main/default/lwc/contactsWithSearch/contactsWithSearch.js
+++ force-app/main/default/lwc/contactsWithSearch/contactsWithSearch.js
@@ -2,14 +2,21 @@ import findContacts from "@salesforce/apex/ContactController.findContacts";
 import { LightningElement, wire } from "lwc";
 
 export default class ContactsWithSearch extends LightningElement {
+  // property to bind what user type into searchbox
   nameToSearch = "";
-
+  // call apex method by passing what user typed as parameter
   @wire(findContacts, { searchKey: "$nameToSearch" })
   contacts;
 
   handleKeyChange(event) {
-    window.clearTimeout(this.delayTimeout);
+    // below line is all we need if we just want to call apex method any time key change
+    // this.nameToSearch = event.target.value ;
 
+    // however making too much apex request before user finish typing is not ideal
+
+    // clear out current delay timeout
+    window.clearTimeout(this.delayTimeout);
+    // store the changed value
     const value = event.target.value;
     // only change the value of nameToSearch
     // if it's within the delay timeout of 300 ms
